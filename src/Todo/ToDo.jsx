1d43810ca9fdf9659f4c01a319fd247a655cd288
samurks <samueltorochagas@gmail.com>
2023-08-25 16:36:07 -0400

    att no toDo
    add função

--- src/Todo/ToDo.jsx
+++ src/Todo/ToDo.jsx
@@ -2,12 +2,17 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function ToDo() {
-   
+   const[atividade, setAtividade] = useState("");
+   const[lista, setLista] = useState([]);
     return (
         <div>
             <h1>Lista de Atividades</h1>
             <Link to="/">home</Link>
-           
+           <form>
+            <input type="text" 
+            onChange={(e)=>{setAtividade(e.target.value)}}/>
+            <button>ADD</button>
+            </form>
         </div>
     );
 }
