933146eda41d833f28d8c3d84f35e64d8e6e4fe3
samurks <samueltorochagas@gmail.com>
2023-08-25 17:26:18 -0400

    att no toDo
    tentei adicionar id na lista

--- src/Todo/ToDo.jsx
+++ src/Todo/ToDo.jsx
@@ -4,12 +4,13 @@ import { Link } from "react-router-dom";
 export default function ToDo() {
    const[atividade, setAtividade] = useState("");
    const[lista, setLista] = useState([]);
-
+const [id, setId] = useState()
    const salvar = (e) => {
     e.preventDefault();
     setLista([...lista,{
-       atividade:atividade}]);
+       atividade:atividade, id:id}]);
         console.log(lista)
+        setId(1+1)
     };
 
     return (
@@ -18,14 +19,12 @@ export default function ToDo() {
             <Link to="/">home</Link>
 
            
-
-           
            <form onSubmit={salvar}>
             <input type="text" 
             onChange={(e)=>{setAtividade(e.target.value)}}/>
             <button>ADD</button>
             </form>
-
+            {id.map((id)=> <p>{id.id}</p>)}
             {lista.map((ativ)=> <p>{ativ.atividade}</p>)}
         </div>
     );
