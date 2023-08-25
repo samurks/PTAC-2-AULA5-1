49adad5c9a0c5fca04993f9dafcdcd961c3529f1
samurks <samueltorochagas@gmail.com>
2023-08-25 16:46:41 -0400

    att no toDo
    adicionar alert salvar

--- src/Todo/ToDo.jsx
+++ src/Todo/ToDo.jsx
@@ -4,11 +4,19 @@ import { Link } from "react-router-dom";
 export default function ToDo() {
    const[atividade, setAtividade] = useState("");
    const[lista, setLista] = useState([]);
+
+   const salvar = ( ) => {
+    alert("Salvar");
+   }
+
     return (
         <div>
             <h1>Lista de Atividades</h1>
+
+            <p>{atividade}</p>
+            
             <Link to="/">home</Link>
-           <form>
+           <form onSubmit={salvar}>
             <input type="text" 
             onChange={(e)=>{setAtividade(e.target.value)}}/>
             <button>ADD</button>
